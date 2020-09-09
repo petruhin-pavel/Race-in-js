ade87d81b5640efc78c4f06097788aa19fe95143
Pavel <64465001+ilbuild@users.noreply.github.com>
2020-09-09 20:41:25 +0300

    2 lesson

--- main.js
+++ main.js
@@ -28,21 +28,64 @@ const keys = {
 const setting = {
   start: false,
   score: 0,
-  speed: 3
+  speed: 3,
+  traffic: 3
 };
+
+// вычисляем оптимальное количество линий
+function getQuantityElements(heightElement) {
+  return document.documentElement.clientHeight / heightElement + 1;
+}
 
 // Объявляем функции ========
 function startGame() {
   start.classList.add('hide');
+
+  for (let i = 0; i < getQuantityElements(100); i++) {
+    const line = document.createElement('div');
+    line.classList.add('line');
+    line.style.top = (i * 100) + 'px';
+    line.y = i * 100;
+    gameArea.appendChild(line);
+  }
+
+  for (let i = 0; i < getQuantityElements(100 * setting.traffic); i++) {
+    const enemy = document.createElement('div');
+    enemy.classList.add('enemy');
+    enemy.y = -100 * setting.traffic * (i + 1);
+    enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+    enemy.style.top = enemy.y + 'px';
+    enemy.style.background = 'transparent url("./image/enemy2.png") center / cover no-repeat';
+    gameArea.appendChild(enemy);
+  }
+
   setting.start = true;
   gameArea.appendChild(car); // вставить дочерний элемент
+  setting.x = car.offsetLeft; // Управление автомобилем с помощью отступа слева
+  setting.y = car.offsetTop;
   requestAnimationFrame(playGame); //современный метод анимации
 
 }
 
 function playGame() {
   // Если (пока) setting.start строго равен true, перезапускается анимация
-  if (setting.start) /* (setting.start === true) тоже самое */ {
+  if (setting.start) { // (setting.start === true) тоже самое
+    moveRoad(); // движение дороги
+    moveEnemy();
+    if (keys.ArrowLeft && setting.x > 0) { // (keys.ArrowLeft === true)
+      setting.x -= setting.speed; // если кнопка зажата, уменьшаем значение х (-= опер. присвоение setting.x = setting.x - setting.speed)
+    }
+    if (keys.ArrowRight && setting.x < (gameArea.offsetWidth - car.offsetWidth)) { // 2е условие - ограничение передвижения, получаем ширину дороги - ширина авто
+      setting.x += setting.speed; // если кнопка зажата, увеличиваем значение х
+    }
+    if (keys.ArrowUp && setting.y > 0) {
+      setting.y -= setting.speed;
+    }
+    if (keys.ArrowDown && setting.y < (gameArea.offsetHeight - car.offsetHeight)) {
+      setting.y += setting.speed;
+    }
+    car.style.left = setting.x + 'px'; // Добавляем результат в стили
+    car.style.top = setting.y + 'px';
     requestAnimationFrame(playGame);
   }
 
@@ -56,4 +99,31 @@ function startRun(event) {
 function stopRun(event) {
   event.preventDefault();
   keys[event.key] = false; // реакция на нажате кнопки
+}
+
+function moveRoad() {
+  let lines = document.querySelectorAll('.line'); // получаем все линии
+  lines.forEach(function (line) {
+    line.y += setting.speed;
+    line.style.top = line.y + 'px';
+
+    if (line.y >= document.documentElement.clientHeight) {
+      line.y = -100;
+    }
+
+  });
+}
+
+function moveEnemy() {
+  let enemy = document.querySelectorAll('.enemy');
+  enemy.forEach(function (item) {
+    item.y += setting.speed / 2;
+    item.style.top = item.y + 'px';
+    if (item >= document.documentElement.clientHeight) {
+      item.y = -100 * setting.traffic;
+      item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+    }
+  });
+
+
 }
